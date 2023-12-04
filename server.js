027976fb9d2213c80e7807dfe62d7d93869c52dc
Replit user <>
2023-12-04 02:36:50 +0000

    set up template engine

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
